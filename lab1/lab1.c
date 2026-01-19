5f487dc75f247204d17ddb7cd2fb18e941740c72
Timothy Shyong <tjs14@sfu.ca>
2026-01-18 16:08:17 -0800

    can read user input

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,7 +1,24 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-  printf("Hello CMPT 201 World!\n");
-  printf("This is gonna be a wild ride!\n");
+  char *buff = NULL;
+  size_t size = 0;
+  ssize_t num_char;
+
+  printf("Please enter a line of text: ");
+  num_char = getline(&buff, &size, stdin);
+
+  if (num_char == -1) {
+    // add error handling here
+  }
+
+  printf("User entered: %s", buff);
+  printf("number of characters from getline() = %zd\n", num_char);
+  // use buff
+  free(buff);
+
   return 0;
 }
